252ce61175efb336f4e3c5059a1becfd7d0e1f22
William Belle <william.belle@gmail.com>
2014-02-19 19:56:51 +0100

    add logic to insert movies in couchDB

--- bin/add-movies-to-couchdb.js
+++ bin/add-movies-to-couchdb.js
@@ -4,7 +4,6 @@
  */
 
 var fs = require('fs');
-var path = require('path');
 var cradle = require('cradle');
 var colors = require('colors');
 var config = require('../configuration/configuration');
@@ -16,9 +15,32 @@ db.exists(function (err, exists) {
 	if (err) {
 		console.log('Error : ', err);
 	} else if (exists) {
-		console.log('Database already exists'.green);
+		console.log('Database already exists'.red);
 	} else {
 		console.log('Creating database'.green);
 		db.create();
+		addMovies();
 	}
 });
+
+function addMovies(){	
+	var directory = process.argv[2];
+	
+	// list files in directory
+	var files = fs.readdirSync(directory);
+	for (var i=0; i<files.length; i++){
+		
+		// read file and parse content
+		var content = fs.readFileSync(directory+files[i], 'utf8');
+		content = JSON.parse(content);
+		
+		// insert movie in couchDB
+		db.save(content.id.toString(), content, saveCouchDBError);
+	}
+}
+
+function saveCouchDBError(err, res){
+	if (err){
+		console.log('Error : ', err);
+	}
+}
