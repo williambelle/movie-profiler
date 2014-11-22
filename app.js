e23eb6970a6a2dc342bb77576273f557b72b82fe
William Belle <william.belle@gmail.com>
2014-11-22 17:13:47 +0100

    replace bootstrap admin template

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var http = require('http');
 var path = require('path');
 var cradle = require('cradle');
 var config = require('./configuration/configuration');
+var async  = require('async');
 
 var app = express();
 
@@ -31,6 +32,7 @@ var db = connection.database(config.db.name);
 
 //"setup"
 express.request.db = express.response.db = db;
+express.request.async = express.response.async = async;
 
 // development only
 if ('development' == app.get('env')) {
--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,29 @@
  */
 
 exports.index = function(req, res){
-	console.log(req.db);
-	req.db.view('titles/titles', function (err, data) {
-		var toDisplay = [];
-		data.forEach(function(key, value) {
-			value.title = key;
-			toDisplay.push(value);
-		});
-		res.render('index', {table : toDisplay});
+	req.async.parallel([
+		function(callback){
+			req.db.view('nb_movie/nb_movie', function (err, data) {
+				callback(null, data[0].value);
+			});
+		},
+		function(callback){
+			req.db.view('genres/genres', {group: true, reduce: true}, function (err, data) {
+				callback(null, data.length);
+			});
+		},
+		function(callback){
+			req.db.view('topics/topics', {group: true, reduce: true}, function (err, data) {
+				callback(null, data.length);
+			});
+		},
+		function(callback){
+			req.db.view('myrating/myrating', {group: true, reduce: true}, function (err, data) {
+				callback(null, data[0].value);
+			});
+		}
+	], function(err, data){
+		
+		res.render('index', {nbMovie : data[0], nbGenre: data[1], nbTopic: data[2], myrating: data[3]});
 	});
-	
 };
